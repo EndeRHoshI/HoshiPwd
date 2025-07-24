4e67723867fbc5a8bb823307d909eba2cf15e519
lvqx <lvqixing@ddpai.com>
2025-07-24 18:29:47 +0800

    :art: targetSdk 提升到 35；提升 Compose 版本

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.hoshi.pwd"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.hoshi.pwd"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
@@ -56,11 +56,11 @@ dependencies {
     implementation("com.google.code.gson:gson:2.10")
 
     // compose 相关依赖
-    val composeVersion = "1.6.0"
+    val composeVersion = "1.8.2"
     implementation("androidx.compose.ui:ui:$composeVersion")
     implementation("androidx.compose.material:material:$composeVersion")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation("androidx.compose.material3:material3-android:1.2.0")
+    implementation("androidx.activity:activity-compose:1.10.1")
+    implementation("androidx.compose.material3:material3-android:1.3.2")
     debugImplementation("androidx.compose.ui:ui-tooling")
 
     // Room components
